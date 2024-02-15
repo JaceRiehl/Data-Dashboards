03977ab3c3bb1b6044922d61e8d4b4e8f8aad02e
jace.riehl <jace.riehl@gmail.com>
2024-02-14 18:34:55 -0700

    working model

--- credit_card_interest/generate_debt_payoff_stategies.py
+++ credit_card_interest/generate_debt_payoff_stategies.py
@@ -1,7 +1,7 @@
 
 import pandas as pd
 
-debts = [
+debts_snowball = [
     {
         "monthly_payment": 10,
         "total_owed": 2000,
@@ -20,41 +20,75 @@ debts = [
     {
         "monthly_payment": 10,
         "total_owed": 10000,
-        "interest_rate": 0.5,
+        "interest_rate": 0.3,
     },
 ]
 
-debt_df = pd.DataFrame(data = debts)
+debts_avalance = [
+    {
+        "monthly_payment": 10,
+        "total_owed": 2000,
+        "interest_rate": 0.15,
+    },
+    {
+        "monthly_payment": 10,
+        "total_owed": 3000,
+        "interest_rate": 0.20,
+    },
+    {
+        "monthly_payment": 10,
+        "total_owed": 5000,
+        "interest_rate": 0.25,
+    },
+    {
+        "monthly_payment": 10,
+        "total_owed": 10000,
+        "interest_rate": 0.3,
+    },
+]
+
+debt_df = pd.DataFrame(data = debts_snowball)
 
 print(debt_df)
-def snowball_method(debts, amount_put_towards_debts_monthly):
+def payoff_debt(debts, amount_put_towards_debts_monthly, snowball_method = True):
+    print(debts)
     res = {
         'month': [0],
         'total_remaining': [calc_total_debt_remaining(debts)],
         'interest_paid': [0],
     }
-    print(res)
-    debts = sorted(debts, key=lambda x: x["total_owed"])
+    if snowball_method:
+        debts = sorted(debts, key=lambda x: x["total_owed"])
+    else:
+        debts = sorted(debts, key=lambda x: x["interest_rate"])[::-1]
     month = 0
-    interest_paid = 0
-    for debt in debts:
+    
+    for i in range(len(debts)):
         monthly_amount_left_over = 0
-        total_remaining = debt["total_owed"]
-        while debt["total_owed"] > 0:
+        total_remaining = debts[i]["total_owed"]
+        while debts[i]["total_owed"] > 0:
+            # add interest
+            interest = add_interest_on_all_debts(debts)
+            
             month += 1
             # take away monthly amount
-            remaining = debt['total_owed'] - amount_put_towards_debts_monthly - monthly_amount_left_over
+            remaining = debts[i]['total_owed'] - amount_put_towards_debts_monthly - monthly_amount_left_over
             if remaining < 0:
                 monthly_amount_left_over = abs(remaining)
             
-            # add interest
-            interest = debt['total_owed'] * (debt['interest_rate'] / 12)
             
             # append all the calculated_values
-            res['total_remaining'].append(res['total_remaining'][-1] - monthly_amount_left_over)
+            res['total_remaining'].append(calc_total_debt_remaining(debts))
             res['month'].append(month)
             res['interest_paid'].append(res['interest_paid'][-1] + interest)
             
+            # reduce debt
+            if debts[i]['total_owed'] - amount_put_towards_debts_monthly < 0:
+                debts[i]['total_owed'] = 0
+            else:
+                debts[i]['total_owed'] = debts[i]['total_owed'] - amount_put_towards_debts_monthly
+    return res
+
 def calc_total_debt_remaining(debts):
     total_debts = 0
     for i in debts:
@@ -62,13 +96,14 @@ def calc_total_debt_remaining(debts):
     return total_debts
 
 def add_interest_on_all_debts(debts):
+    total_interest_added = 0
     for i in debts:
-        i['total_owed'] += round(i['total_owed'] * (i['interest_rate'] / 12))
-    return debts
+        interest = round(i['total_owed'] * (i['interest_rate'] / 12))
+        i['total_owed'] += interest
+        total_interest_added += interest
+    return interest
         
-# snowball_method(debts, 500)
-        
-add_interest_on_all_debts(debts)
-        
-# want a df of the starting debts
-# second df of the month, total_remaining, interest paid
+
+print(payoff_debt(debts_snowball, 1000, True))
+print("\n\n\n\n\n")
+print(payoff_debt(debts_avalance, 1000, False))
